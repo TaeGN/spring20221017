67d4f41fe0aa5ce2f085d17c0530ff5464c58ddf
TaeGN <110441723+TaeGN@users.noreply.github.com>
2022-11-03 17:43:00 +0900

    20221103

---
+++ src/main/java/org/zerock/domain/lecture/JavaBean21.java
@@ -0,0 +1,13 @@
+package org.zerock.domain.lecture;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class JavaBean21 {
+	private int age;
+	private boolean hasCar;
+	private String name;
+	private List<String> food;
+}
