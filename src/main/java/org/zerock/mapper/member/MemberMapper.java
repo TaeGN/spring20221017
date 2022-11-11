8611838898ac33c8c7b0ee00000878b5373197e8
TaeGN <110441723+TaeGN@users.noreply.github.com>
2022-11-11 10:38:26 +0900

    20221111

---
+++ src/main/java/org/zerock/mapper/member/MemberMapper.java
@@ -0,0 +1,14 @@
+package org.zerock.mapper.member;
+
+import java.util.List;
+
+import org.zerock.domain.member.MemberDto;
+
+public interface MemberMapper {
+
+	int insert(MemberDto member);
+
+	List<MemberDto> selectAll();
+	
+	
+}
